884b99846624e6980d1e7cdad9aa283a158640c2
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-07 15:52:16 +0530

    even or odd

---
+++ script.js
@@ -0,0 +1,12 @@
+//even or odd 
+
+const num = prompt('enter the number ');
+
+//even 
+if(num % 2 ==0){
+console.log("the number is even");
+}
+//odd
+else{
+    console.log("the number is odd ");
+}
